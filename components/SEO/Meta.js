df5e22c77dc2abd2da943cbd447432131c13269f
Karo <76846752+InvalidKaro@users.noreply.github.com>
2023-10-01 14:00:03 +0200

    Update Meta.js

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -22,22 +22,22 @@ export default function Meta() {
             /* Search Engine */
             <meta name="image" content="images/logos/fevicon.png" />
             /* Schema.org for Google */
-            <meta itemProp="name" content="Invalid Karo Portfolio - Fullstack Developer - Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
+            <meta itemProp="name" content="Invalid Karo Portfolio - Fullstack Developer" />
             <meta itemProp="description"
-                content="Invalid Karo's (invalidkaro) Personal Portfolio Website - Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
+                content="Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
             <meta itemProp="image" content="images/logos/fevicon.png" />
             /* Twitter */
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content="Invalid Karo Portfolio - Fullstack Developer" />
             <meta name="twitter:description"
-                content="Invalid Karo's (invalidkaro) Personal Portfolio Website - Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
+                content="Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
             <meta name="twitter:site" content="invalidkaro" />
             <meta name="twitter:creator" content="invalidkaro" />
             <meta name="twitter:image" content="https://iili.io/JdWnTzX.md.png" />
             /* Open Graph general (Facebook, Pinterest & Google+) */
             <meta name="og:title" content="Invalid Karo Portfolio - Fullstack Developer" />
             <meta name="og:description"
-                content="Invalid Karo's (invalidkaro) Personal Portfolio Website - Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
+                content="Discover my personal portfolio website designed with the Ubuntu 20.04 theme, created using Next.js and Tailwind CSS. Explore my projects, contributions, and more." />
             <meta name="og:image" content="https://www.iili.io/JdWnTzX.md.png" />
             <meta name="og:url" content="http://invalidkaro.github.io/" />
             <meta name="og:site_name" content="Invalid Karo Personal Portfolio" />
